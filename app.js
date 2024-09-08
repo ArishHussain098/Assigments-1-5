9075df50eb0fa396bece6b899935d3425c5df3df
ArishHussain <arishhissain098@gmail.com>
2024-09-08 15:07:16 +0500

    commit out

--- app.js
+++ app.js
@@ -1,85 +1,85 @@
-// var username = "ArishHussain"
-// var num = 28
-//  console.log("my name is" + username , "and my age is" + num);
+var username = "ArishHussain"
+var num = 28
+ console.log("my name is" + username , "and my age is" + num);
 
-        //   ALERT
-/////////   Assignments 01 ////////////////
-// 02
-//  alert("Error!Please enter a valid password")
-// // 03
-//  alert("Welcome to JS land..." +"\n"+ "Happy codding")
-// // 04
-// alert("Welcome to JS land...")
-// alert("Happy codding")
-// 05
-
-
-        //VAREABLR
-
-//  01
-// var username = ""  
-// 02 
-// var username = "Arish hussain" 
+          ALERT
+///////   Assignments 01 ////////////////
+02
+ alert("Error!Please enter a valid password")
 // 03
-// var massage = "Hello World" 
-// alert(massage)
+ alert("Welcome to JS land..." +"\n"+ "Happy codding")
 // 04
-// var username="Arish hussain"
-// alert(username)
-// var class1="B.com"
-// alert(class1)
-// var age="my age is 28"
-// alert(age)
-// 05
-// var username = "pizza"+"\n"+"pizz"+"\n"+"piz"+"\n"+"pi";
-// alert(username)
-// var greetuser ="My email id"
-// var email ="arishshaikh098@gmail.com"
-// alert(greetuser+" "+email)
-// 06
-// var book = "I am traying to learn from the Book "+"\n"+"A smarter way to learn javascript "
-// alert(book)
-// 07
-// var greetuser ="yah! I can write HTML" 
-// document.write(greetuser)
-
-         //VARIABLE OF NUMBER 
-// 01
-// var age = 28
-// alert("i am "+ " " + age +" "+"year old")
-// 02
-// var age = "My birth year is 1996 "
-// document.write(age)
-// 03
-// var brand =prompt("Enter your brand name")
-// var username =prompt("Username")
-// var quantity =prompt("Enter your quantity")
-// document.write(username+brand+quantity)
+alert("Welcome to JS land...")
+alert("Happy codding")
+05
 
 
+        VAREABLR
 
-// var  a =3
-// var  b =5
-// var  c =a+b
-// document.write("sum of"+" "+a+"and"+b+"is equal to "+c)
+ 01
+var username = ""  
+02 
+var username = "Arish hussain" 
+03
+var massage = "Hello World" 
+alert(massage)
+04
+var username="Arish hussain"
+alert(username)
+var class1="B.com"
+alert(class1)
+var age="my age is 28"
+alert(age)
+05
+var username = "pizza"+"\n"+"pizz"+"\n"+"piz"+"\n"+"pi";
+alert(username)
+var greetuser ="My email id"
+var email ="arishshaikh098@gmail.com"
+alert(greetuser+" "+email)
+06
+var book = "I am traying to learn from the Book "+"\n"+"A smarter way to learn javascript "
+alert(book)
+07
+var greetuser ="yah! I can write HTML" 
+document.write(greetuser)
 
-// var a = 5;
-// var b = ++a;
-// var c = ++b
-// console.log(c);
+         VARIABLE OF NUMBER 
+01
+var age = 28
+alert("i am "+ " " + age +" "+"year old")
+02
+var age = "My birth year is 1996 "
+document.write(age)
+03
+var brand =prompt("Enter your brand name")
+var username =prompt("Username")
+var quantity =prompt("Enter your quantity")
+document.write(username+brand+quantity)
 
 
 
-                ////  if else  //////
-// question no 1
-// var a= +prompt("enter your num")
-// if(a==3){
-//     console.log("it is divisible");    
-// }
-// else{
-//     console.log("itnis not dividible");  
-// }
-// question no2
+var  a =3
+var  b =5
+var  c =a+b
+document.write("sum of"+" "+a+"and"+b+"is equal to "+c)
+
+var a = 5;
+var b = ++a;
+var c = ++b
+console.log(c);
+
+
+
+                //  if else  //////
+question no 1
+var a= +prompt("enter your num")
+if(a==3){
+    console.log("it is divisible");    
+}
+else{
+    console.log("itnis not dividible");  
+}
+question no2
 
 function checknum(number) {
     if (number % 2 === 0) {
