f7c091bab75e89ef8ccbfb00c152f48b6c4e9a89
ArishHussain <arishhissain098@gmail.com>
2024-09-08 15:10:19 +0500

    error problem solve

--- app.js
+++ app.js
@@ -4,53 +4,53 @@ var num = 28
 
           ALERT
 ///////   Assignments 01 ////////////////
-02
+// 02
  alert("Error!Please enter a valid password")
 // 03
  alert("Welcome to JS land..." +"\n"+ "Happy codding")
 // 04
 alert("Welcome to JS land...")
 alert("Happy codding")
-05
+// 05
 
 
         VAREABLR
 
- 01
+//  01
 var username = ""  
-02 
+// 02 
 var username = "Arish hussain" 
-03
+// 03
 var massage = "Hello World" 
 alert(massage)
-04
+// 04
 var username="Arish hussain"
 alert(username)
 var class1="B.com"
 alert(class1)
 var age="my age is 28"
 alert(age)
-05
+// 05
 var username = "pizza"+"\n"+"pizz"+"\n"+"piz"+"\n"+"pi";
 alert(username)
 var greetuser ="My email id"
 var email ="arishshaikh098@gmail.com"
 alert(greetuser+" "+email)
-06
+// 06
 var book = "I am traying to learn from the Book "+"\n"+"A smarter way to learn javascript "
 alert(book)
-07
+// 07
 var greetuser ="yah! I can write HTML" 
 document.write(greetuser)
 
-         VARIABLE OF NUMBER 
-01
+        //  VARIABLE OF NUMBER 
+// 01
 var age = 28
 alert("i am "+ " " + age +" "+"year old")
-02
+// 02
 var age = "My birth year is 1996 "
 document.write(age)
-03
+// 03
 var brand =prompt("Enter your brand name")
 var username =prompt("Username")
 var quantity =prompt("Enter your quantity")
@@ -71,7 +71,7 @@ console.log(c);
 
 
                 //  if else  //////
-question no 1
+// question no 1
 var a= +prompt("enter your num")
 if(a==3){
     console.log("it is divisible");    
@@ -79,7 +79,7 @@ if(a==3){
 else{
     console.log("itnis not dividible");  
 }
-question no2
+// question no2
 
 function checknum(number) {
     if (number % 2 === 0) {
